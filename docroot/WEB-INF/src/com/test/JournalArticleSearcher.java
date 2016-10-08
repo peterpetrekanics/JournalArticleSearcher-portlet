3f0c4136298e2d0ac08d94be0d7b55354a944d07
petrep <petrep@freemail.hu>
2016-10-08 15:04:57 +0200

    Cleaned up the code and added comments, source formatting

--- docroot/WEB-INF/src/com/test/JournalArticleSearcher.java
+++ docroot/WEB-INF/src/com/test/JournalArticleSearcher.java
@@ -42,33 +42,24 @@ public class JournalArticleSearcher extends MVCPortlet {
 			ActionResponse actionResponse) throws IOException, PortletException {
 		System.out.println("* ProcessAction starts.. *");
 
+		// Defines basic variables
 		final long companyId = PortalUtil.getDefaultCompanyId();
 		long defGroupId = 0;
-		long groupId = 0;
 		try {
 			defGroupId = GroupLocalServiceUtil.getGroup(companyId, "Guest")
 					.getGroupId();
-			// groupId = GroupLocalServiceUtil.getGroup(companyId,
-			// "TestSite1").getGroupId();
 		} catch (PortalException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (SystemException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
-		// final Role siteMemberRole = RoleLocalServiceUtil.getRole(companyId,
-		// RoleConstants.SITE_MEMBER);
 		User currentUser = null;
-		List<Role> fetchedRoles = null;
 		try {
 			currentUser = PortalUtil.getUser(actionRequest);
 		} catch (PortalException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		} catch (SystemException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		ThemeDisplay themeDisplay = (ThemeDisplay) actionRequest
@@ -80,148 +71,51 @@ public class JournalArticleSearcher extends MVCPortlet {
 		PermissionChecker permissionChecker = themeDisplay
 				.getPermissionChecker();
 
-		try {
-			fetchedRoles = getRole(currentUser);
-		} catch (SystemException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (PortalException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
 		System.out.println("Username of the currently logged on user: "
 				+ currentUser.getScreenName());
-		for (Role fetchedRole : fetchedRoles) {
-			System.out.println("roles> " + fetchedRole.getName());
-		}
 
-//		int start = 0;
-//		int end = 1000;
-//		List<JournalArticle> myResults1 = new ArrayList<JournalArticle>();
-//		List<JournalArticle> myResults2 = new ArrayList<JournalArticle>();
-		//try {
-//			myResults1 = JournalArticleLocalServiceUtil.getArticles(defGroupId,
-//					start, end);
-//			myResults1 = JournalArticleLocalServiceUtil.getArticles(defGroupId);
-			// myResults2 = JournalArticleLocalServiceUtil.getArticles(groupId,
-			// start, end);
-//		} catch (SystemException e) {
-//			e.printStackTrace();
-//		}
-		
-		
+		// Collects a list of journal articles, and this list will only contain the latest version of the articles.
 		String myStructureKey = "";
 		JournalArticle articleLastVersion;
 		List<JournalArticle> latestArticleList = new ArrayList<JournalArticle>();
 		List<JournalArticle> allArticlesList = null;
 		try {
-			allArticlesList = JournalArticleLocalServiceUtil.getStructureArticles(defGroupId, myStructureKey);
+			allArticlesList = JournalArticleLocalServiceUtil
+					.getStructureArticles(defGroupId, myStructureKey);
 		} catch (SystemException e1) {
-			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
 		ListIterator<JournalArticle> it = allArticlesList.listIterator();
 		List<String> checkedArticleIds = new ArrayList<String>();
-		 
-		while (it.hasNext()) {
-		  JournalArticle article = it.next();
-		  if (checkedArticleIds.contains(article.getArticleId())) {
-		    continue; // previous article version already checked
-		  }
-		  try {
-			articleLastVersion = JournalArticleLocalServiceUtil.getLatestArticle(defGroupId, article.getArticleId());
-		} catch (PortalException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SystemException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-//		  checkedArticleIds.add(article.getArticleId());
-//		  System.out.println("Added articleId " + article.getArticleId() + " with version " + article.getVersion());
-		  latestArticleList.add(article);
-		}
-		
-		
-		
-		for (JournalArticle myResult1 : latestArticleList) {
 
+		while (it.hasNext()) {
+			JournalArticle article = it.next();
+			if (checkedArticleIds.contains(article.getArticleId())) {
+				continue; // previous article version already checked
+			}
+			try {
+				articleLastVersion = JournalArticleLocalServiceUtil
+						.getLatestArticle(defGroupId, article.getArticleId());
+				latestArticleList.add(article);
+			} catch (PortalException e) {
+				e.printStackTrace();
+			} catch (SystemException e) {
+				e.printStackTrace();
+			}
+			checkedArticleIds.add(article.getArticleId());
+		}
+
+		// Loops through the latest article list, and checks if the currently logged on user has view permissions
+		for (JournalArticle myResult1 : latestArticleList) {
 			try {
 				JournalArticlePermission.check(permissionChecker, myResult1,
 						ActionKeys.VIEW);
-//				System.out.println("---myResult START: " + myResult1.getTitle());
-				System.out.println(myResult1.getTitle());
-			} catch (PortalException e) {
-				// TODO Auto-generated catch block
-//				System.out.println("NO PERMISSIONS!!");
-				// e.printStackTrace();
+				System.out.println("The user has only view permissions on this article:");
+				System.out.println(myResult1.getTitleCurrentValue());
+				} catch (PortalException e) {
 			}
-
-			// System.out.println("art " + myResult1.getTitle());
-
-//			System.out.println("---myResult END ");
-
 		}
-
-		// final long roleId = RoleLocalServiceUtil.getRole(companyId,
-		// RoleConstants.GUEST).getRoleId();
-		// final Map<Long, String[]> roles = new HashMap<Long, String[]>();
-		// roles.put(roleId, new String[] { ActionKeys.VIEW });
-		// if
-		// (!ResourcePermissionLocalServiceUtil.hasResourcePermission(companyId,
-		// JournalArticle.class.getName(),
-		// ResourceConstants.SCOPE_INDIVIDUAL,
-		// Long.toString(model.getResourcePrimKey()), roleId, ActionKeys.VIEW))
-		// {
-		// ResourcePermissionLocalServiceUtil.setResourcePermissions(companyId,
-		// JournalArticle.class.getName(),
-		// ResourceConstants.SCOPE_INDIVIDUAL,
-		// Long.toString(model.getResourcePrimKey()), roles);
-		// }
-
-		// for(JournalArticle myResult2 : myResults2){
-		// System.out.println("---myResult2: " + myResult2);
-		// }
 
 		System.out.println("* ProcessAction ends.. *");
 	}
-
-	private List<Role> getRole(User currentUser) throws SystemException,
-			PortalException {
-		List<Role> roles = new ArrayList<Role>();
-		roles.addAll(currentUser.getRoles());
-		roles.addAll(getUserGroupRolesOfUser(currentUser));
-		roles.addAll(getUserExplicitRoles(currentUser));
-		return roles;
-	}
-
-	List<Role> getUserExplicitRoles(User user) throws SystemException,
-			PortalException {
-		List<Role> roles = new ArrayList<Role>();
-		List<UserGroupRole> userGroupRoles = UserGroupRoleLocalServiceUtil
-				.getUserGroupRoles(user.getUserId());
-		for (UserGroupRole userGroupRole : userGroupRoles) {
-			roles.add(userGroupRole.getRole());
-		}
-		return roles;
-	}
-
-	private static List<Role> getUserGroupRolesOfUser(User user)
-			throws SystemException, PortalException {
-		List<Role> roles = new ArrayList<Role>();
-		List<UserGroup> userGroupList = UserGroupLocalServiceUtil
-				.getUserUserGroups(user.getUserId());
-		List<UserGroupGroupRole> userGroupGroupRoles = new ArrayList<UserGroupGroupRole>();
-		for (UserGroup userGroup : userGroupList) {
-			userGroupGroupRoles.addAll(UserGroupGroupRoleLocalServiceUtil
-					.getUserGroupGroupRoles(userGroup.getUserGroupId()));
-		}
-		for (UserGroupGroupRole userGroupGroupRole : userGroupGroupRoles) {
-			Role role = RoleLocalServiceUtil.getRole(userGroupGroupRole
-					.getRoleId());
-			roles.add(role);
-		}
-		return roles;
-	}
-
 }
